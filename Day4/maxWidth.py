e9ee6c0ed1bfc73f6747a2978bd27b6d8055cf18
Shivansh Srivastava <srivastavashiv0112@gmail.com>
2024-01-13 20:40:30 +0530

    Day4

---
+++ Day4/maxWidth.py
@@ -0,0 +1,38 @@
+"""1637. Widest Vertical Area Between Two Points Containing No Points
+Solved
+Easy
+Topics
+Companies
+Hint
+Given n points on a 2D plane where points[i] = [xi, yi], Return the widest vertical area between two points such that no points are inside the area.
+
+A vertical area is an area of fixed-width extending infinitely along the y-axis (i.e., infinite height). The widest vertical area is the one with the maximum width.
+
+Note that points on the edge of a vertical area are not considered included in the area.
+
+ 
+
+Example 1:
+
+​
+Input: points = [[8,7],[9,9],[7,4],[9,7]]
+Output: 1
+Explanation: Both the red and the blue area are optimal.
+Example 2:
+
+Input: points = [[3,1],[9,0],[1,0],[1,4],[5,3],[8,8]]
+Output: 3
+ 
+
+Constraints:
+
+n == points.length
+2 <= n <= 105
+points[i].length == 2
+0 <= xi, yi <= 109"""
+
+class Solution:
+    def maxWidthOfVerticalArea(self, points: List[List[int]]) -> int:
+        ar = [x for x, _ in sorted(points)]
+        return max([ar[i+1] - ar[i] for i in range(len(ar)-1)])
+
